52bbc9aab6b49bd211afed655d61a13285c84750
Piyush Nashikkar <piyushnas74@gmail.com>
2025-11-17 13:02:47 +0530

    undo changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,12 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 5173,
-  },
 });
